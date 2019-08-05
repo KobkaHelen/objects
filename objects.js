e4b0a03f0389531a7bfaaa12c8a0d1d37a22761d
Elena <helen4helen7@gmail.com>
2019-08-05 08:39:51 +0300

    objects

---
+++ objects.js
@@ -0,0 +1,142 @@
+// Вікно
+const window = {
+    width: 120,
+    height: 160,
+    glasses: 6
+}
+console.log(window, window.height)
+
+// Небо
+const sky = {
+    color: "blue",
+    clouds: 3
+}
+console.log(sky)
+
+// місяць
+const moon = {
+    view: "full",
+    zodiacSign: "lion" 
+}
+console.log(moon)
+
+//сонце
+const sun = {
+    distance: 149600000, 
+    heatTransfer: 1500 
+}
+console.log(sun)
+
+//зошит
+const notebook = {
+    numberOfSheets: 48,
+    sheetView: "into the cage"
+}
+console.log(notebook)
+
+// автомобіль
+const car = {
+    weight: 2476,
+    speed: 215
+}
+console.log(car)
+
+//мотоцикл
+const motorbike = {
+    profile: 'GLR125F(CB125F) - 2018',
+    speed: 100,
+    weight: 128
+}
+console.log(motorbike)
+
+//літак
+const airplane = {
+    weight: 162,
+    numberОfSeats: 600,
+    enginePower: 110000
+}
+console.log(airplane)
+
+//човен
+const ship = {
+    material: "rubber",
+    quantitySeats: 4
+}
+console.log(ship)
+
+//велосипед
+const bicycle = {
+    view: "sport",
+    rubber: "spikes"
+}
+console.log(bicycle)
+
+// кішка
+const cat = {
+    species: "sphinx",
+    gender: "female"
+}
+console.log(cat)
+
+//людина
+const human = {
+    age: 33,
+    gender: "women"
+}
+console.log(human)
+
+//гриб
+const mushroom = {
+    variety: "champignon",
+    size: "large"
+}
+console.log(mushroom)
+
+//квітка
+const flowers = {
+    colors: ['white','yellow'],
+    species: 'camomile'
+}
+console.log(flowers)
+
+//мураха
+const ant = {
+    color: 'braun',
+    species: 'forest'
+}
+console.log(ant)
+
+// мрія
+const dream = {
+    possibility: "is present",
+    realization: "in the process"
+}
+console.log(dream)
+
+//думка
+const thought = {
+    kind: "global",
+    type: "positive"
+}
+console.log(thought)
+
+//ємоція
+const emotion = {
+    specificity: 'realization',
+    view: 'neutral'
+}
+console.log(emotion)
+
+//дія
+const act = {
+    view: "completed",
+    result: "positive"
+}
+console.log(act)
+
+//вислів
+const expression = {
+    meaning: "exhaustive",
+    motivation: "be present"
+}
+console.log(expression)
